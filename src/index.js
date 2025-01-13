47d6e4b836983561863a901be69449709f629517
Simone Cecire <ceciresimone@itis-molinari.eu>
2025-01-13 14:27:47 +0100

    aggiornamento home dinamica

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {generateNavigator} from "./scripts/navigatorComponent/navigatorComponent.js";
 import {generateFetchComponent} from "./scripts/fetchComponent/fetchComponent.js";
+import {generateGeoencoder} from "./scripts/geoencoderComponent/geoencoderComponent.js";
 
 const modalBody = document.getElementById("modalBody");
 const tableContainer = document.getElementById("tableContainer");
@@ -8,8 +9,14 @@ const searchbarContainer = document.getElementById("searchbarContainer");
 const mapContainer = document.getElementById("map");
 const loginContainer = document.getElementById("loginContainer");
 
-const navigator = generateNavigator();
+const playsTableHomeBody = document.getElementById("playsTableHomeBody");
+const pages = document.getElementById("pages");
+
+const navigator = generateNavigator(pages);
 const fetchComponent = generateFetchComponent();
+const geoencoder = generateGeoencoder();
+
+const playsTableRowHomeTemplate = '<tr><td><a href="#article" id="%PLAYTITLE">%PLAYTITLE <i class="bi bi-box-arrow-up-right"></i></a></td><td>%PLACE</td></tr>';
 
 fetch("./conf.json")
 .then(r => r.json())
@@ -17,5 +24,21 @@ fetch("./conf.json")
     let cacheToken = data["cacheToken"];
     let mapsToken = data["mapsToken"];
 
+    fetchComponent.build(cacheToken);
+    geoencoder.build(mapsToken);
 
+    let remoteData = {};
+
+    fetchComponent.getData("poi").then(data => {
+        remoteData = data;
+        let newHTML = "";
+
+        let keys = Object.keys(remoteData);
+        let values = Object.values(remoteData);
+
+        for (let i = 0; i < keys.length; i++) {
+            newHTML += playsTableRowHomeTemplate.replaceAll("%PLAYTITLE", keys[i]).replace("%PLACE", values[i].place);
+        }
+        playsTableHomeBody.innerHTML = newHTML;
+    });
 });
