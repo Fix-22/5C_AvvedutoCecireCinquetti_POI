bd7b1f04071a6f791dca1dbe4cf845894db82a1e
Simone Cecire <ceciresimone@itis-molinari.eu>
2025-01-14 15:28:26 +0100

    aggiornamento dinamico pulsanti

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import {generateMap} from "./scripts/mapComponent/mapComponent.js";
 
 const modalBody = document.getElementById("modalBody");
 const tableContainer = document.getElementById("tableContainer");
-const spinner = document.getElementById("spinner");
 const searchbarContainer = document.getElementById("searchbarContainer");
 const loginContainer = document.getElementById("loginContainer");
 
+const spinner = document.getElementById("spinner");
 const playsTableHomeBody = document.getElementById("playsTableHomeBody");
+const playsTableAdminBody = document.getElementById("playsTableAdminBody");
 const pages = document.getElementById("pages");
 const mapContainer = document.getElementById("mapContainer");
 const articleContainer = document.getElementById("article");
@@ -20,6 +21,7 @@ const geoencoder = generateGeoencoder();
 const map = generateMap(mapContainer);
 
 const playsTableRowHomeTemplate = '<tr><td><a href="#article" id="%PLAYTITLE" class="articleLink">%PLAYTITLE <i class="bi bi-box-arrow-up-right"></i></a></td><td>%PLACE</td></tr>';
+const playsTableRowAdminTemplate = '<tr><td>%PLAYTITLE</td><td><button type="button" id="%EDITID" class="btn btn-warning editButton" data-bs-toggle="modal" data-bs-target="#modalForm"><i class="bi bi-pencil-square"></i> Edit</button> <button type="button" id="%DELETEID" class="btn btn-danger deleteButton"><i class="bi bi-trash"></i> Delete</button></td></tr>';
 const articleTemplate = `<div class="container">
         <div class="row d-flex justify-content-center">
             <div class="long-line"></div>
@@ -71,7 +73,8 @@ fetch("./conf.json")
 
     fetchComponent.getData("poi").then(data => {
         remoteData = data;
-        let newHTML = "";
+        let newHomeHTML = "";
+        let newAdminHTML = "";
 
         let keys = Object.keys(remoteData);
         let values = Object.values(remoteData);
@@ -83,9 +86,9 @@ fetch("./conf.json")
                 map.addPlace(place);
                 map.render();
             });
-            newHTML += playsTableRowHomeTemplate.replaceAll("%PLAYTITLE", keys[i]).replace("%PLACE", values[i].place);
+            newHomeHTML += playsTableRowHomeTemplate.replaceAll("%PLAYTITLE", keys[i]).replace("%PLACE", values[i].place);
         }
-        playsTableHomeBody.innerHTML = newHTML;
+        playsTableHomeBody.innerHTML = newHomeHTML;
 
         document.querySelectorAll(".articleLink").forEach(a => {
             a.onclick = () => {
@@ -93,8 +96,28 @@ fetch("./conf.json")
                 articleContainer.innerHTML = articleTemplate.replaceAll("%PLAYTITLE", a.id).replace("%PLACE", currentArticleData.place).replace("%YEAROFPUB", currentArticleData.yearofpub).replace("%ERA", currentArticleData.era).replace("%RESUME", currentArticleData.resume).replace("%CHARACTERS", currentArticleData.characters).replace("%IMGLINK1", currentArticleData.images[0]).replace("%IMGLINK2", currentArticleData.images[1]).replace("%IMGLINK3", currentArticleData.images[2]);
             };
         });
+
+        for (let i = 0; i < keys.length; i++) {
+            newAdminHTML += playsTableRowAdminTemplate.replace("%PLAYTITLE", keys[i]).replace("%DELETEID", "delete-" + keys[i]).replace("%EDITID", "edit-" + keys[i]);
+        }
+        playsTableAdminBody.innerHTML = newAdminHTML;
+
+        document.querySelectorAll(".editButton").forEach(b => {
+            b.onclick = () => {
+                // CODICE PER AGGIUNGERE TESTO AL FORM
+            };
+        });
+
+        document.querySelectorAll(".deleteButton").forEach(b => {
+            b.onclick = () => {
+                const playTitle = b.id.replace("delete-", "");
+                const newLocalData = JSON.parse(JSON.stringify(remoteData));
+                delete newLocalData[playTitle];
+                
+                fetchComponent.setData("poi", newLocalData).then()
+            };
+        });
+
         spinner.classList.add("d-none");
     });
-
-    map.render();
 });
