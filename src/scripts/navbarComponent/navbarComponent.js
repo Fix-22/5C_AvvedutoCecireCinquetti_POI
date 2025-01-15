ac809c6c4caad070d52a50fd8f8be987b0277f80
Luca Avveduto <avvedutoluca@itis-molinari.eu>
2025-01-15 17:23:35 +0100

    nav-start

---
+++ src/scripts/navbarComponent/navbarComponent.js
@@ -0,0 +1,42 @@
+export function navBarComponent(parentElement) {
+    let elements;
+    let callback;
+    return {
+        build: (list) => {
+          elements = list;
+        },
+        render: () => {
+            let item = 
+            ` <li class="nav-item">
+                %item
+            </li>`;
+
+            let newNavBar = 
+            `<nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav w-100 d-flex justify-content-between">`
+                            + (elements.map((e)=> item.replace("%item",e))).join("") +
+                        `</ul>
+                    </div>
+                </div>
+            </nav>`;
+            parentElement.innerHTML = newNavBar;
+        }
+            /*document.querySelector("#open").onclick = () => document.querySelector("#result").innerText = "";
+
+            config.tipologie.forEach(element => document.getElementById(element).onclick = () => {
+                activeNavBar(element, config);
+                callback(element);
+            })
+        },
+        callback(value){
+            callback = value;
+        }*/
+    }
+}
